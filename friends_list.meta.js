7eb156c3f8484f5d37a67e2c3ebd22bc8c36c2c6
Alin Stefan Olaru <alinstefanolaru@gmail.com>
2020-04-28 23:31:49 +0200

    Initial commit

---
+++ friends_list.meta.js
@@ -0,0 +1,30 @@
+// ==UserScript==
+// @name           The West Friends List
+// @description    Returns the friends list replaced by the friends bar.
+// @author         Diggo11
+// @match          https://*.the-west.net/game.php*
+// @match          https://*.the-west.de/game.php*
+// @match          https://*.the-west.pl/game.php*
+// @match          https://*.the-west.nl/game.php*
+// @match          https://*.the-west.se/game.php*
+// @match          https://*.the-west.ro/game.php*
+// @match          https://*.the-west.com.pt/game.php*
+// @match          https://*.the-west.cz/game.php*
+// @match          https://*.the-west.es/game.php*
+// @match          https://*.the-west.ru/game.php*
+// @match          https://*.the-west.com.br/game.php*
+// @match          https://*.the-west.org/game.php*
+// @match          https://*.the-west.hu/game.php*
+// @match          https://*.the-west.gr/game.php*
+// @match          https://*.the-west.dk/game.php*
+// @match          https://*.the-west.sk/game.php*
+// @match          https://*.the-west.fr/game.php*
+// @match          https://*.the-west.it/game.php*
+// @match          http://game.west/game.php*
+// @exclude        https://classic.the-west.net/game.php*
+// @version        4.01
+// @grant          GM_info
+// @run-at         document-start
+// @updateURL      https://west.diggo11.com/userscripts/friends_list.meta.js
+// @downloadURL    https://west.diggo11.com/userscripts/friends_list.user.js
+// ==/UserScript==
